d27c6ab417fae1350f0d76914a3b7a81319f508e
AnTranThienDoan <doaty014@mymail.unisa.edu.au>
2026-09-04 17:03:38 +0930

    Finish making script to pick up key

--- Assets/Scripts/Key_Pickup.cs
+++ Assets/Scripts/Key_Pickup.cs
@@ -1,16 +1,75 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Key_Pickup : MonoBehaviour
+public class KeyPickup : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Pickup Settings")]
+    [SerializeField] private float pickupDistance = 2f;
+
+    [Header("Player")]
+    [SerializeField] private Transform player;
+
+    [Header("UI")]
+    [SerializeField] private GameObject pickupPrompt;
+
+    private void Start()
     {
-        
+        // Automatically find the player
+        if (player == null)
+        {
+            GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+
+            if (playerObject != null)
+            {
+                player = playerObject.transform;
+            }
+        }
+
+        // Hide prompt at the beginning
+        if (pickupPrompt != null)
+        {
+            pickupPrompt.SetActive(false);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (player == null)
+            return;
+
+        float distance = Vector3.Distance(
+            transform.position,
+            player.position
+        );
+
+        // Player is close to the key
+        if (distance <= pickupDistance)
+        {
+            if (pickupPrompt != null)
+                pickupPrompt.SetActive(true);
+
+            // New Input System
+            if (Keyboard.current != null &&
+                Keyboard.current.eKey.wasPressedThisFrame)
+            {
+                PickUpKey();
+            }
+        }
+        else
+        {
+            if (pickupPrompt != null)
+                pickupPrompt.SetActive(false);
+        }
+    }
+
+    private void PickUpKey()
+    {
+        Debug.Log("KEY PICKED UP!");
+
+        if (pickupPrompt != null)
+            pickupPrompt.SetActive(false);
+
+        // Remove the key
+        gameObject.SetActive(false);
     }
 }
